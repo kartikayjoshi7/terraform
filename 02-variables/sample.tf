a48c79f83e3c60109e4b8e0c6137da99b429cb52
Kartikay <kartikayjoshiprivate@gmail.com>
2022-07-25 02:19:51 +0530

    File updated

--- 02-variables/sample.tf
+++ 02-variables/sample.tf
@@ -1,4 +1,5 @@
 variable "demo" {
+  type = string
   default = "Hello World"
 }
 
@@ -7,8 +8,30 @@ output "demo" {
 }
 
 variable "demo1" {
+  default = 100
+  type = number
 }
 
-output "demo1" {
-  value = var.demo1
+variable "demo2" {
+  description = "Demo variable for boolean demo"
+  default = true
+  type = bool
+}
+
+variable "demo3" {
+  description = "list variables"
+  default = [
+    100,
+    "abc"
+  ]
+}
+
+
+variable "demo4" {
+  description = "Map variables"
+  default = {
+    course ="DevOps"
+    timing = "6am"
+    is_started = true
+  }
 }
